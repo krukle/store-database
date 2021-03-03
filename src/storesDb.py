d8f956c4f40a3256e6a1e405d3866dfb5f88d532
Christoffer Eid <ce223af@student.lnu.se>
2021-03-03 16:54:44 +0100

    add finished tables

--- src/storesDb.py
+++ src/storesDb.py
@@ -1,15 +1,17 @@
+from __future__ import print_function
 import mysql.connector
 import csv
-
+from mysql.connector import errorcode
+import random
 
 # Connection info
 cnx = mysql.connector.connect(user='root', password='dGX!f&^HTo5h36',
                                host='127.0.0.1')
 cursor  = cnx.cursor()
-DB_NAME = 'Stores'
+DB_NAME = 'StoreDatabase'
 
 # Source: https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
-def create_database(cursor):
+def create_database(cursor, DB_NAME):
     try:
         cursor.execute(
             "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
@@ -33,8 +35,8 @@ def fillArticleTable(articleList):
     for i in articleList[1:]:
         cursor.execute(f"""
         INSERT INTO 
-            Articles(name, price(sek)) 
-            VALUES({i[0]}, {i[1]})""")
+            `Articles` (`name`, `price`) 
+            VALUES("{i[0]}", "{i[1]}")""")
 
 # Fill Customer table from list
 # Skipping first entry as we dont want headlines as entries.
@@ -42,8 +44,8 @@ def fillCustomerTable(customerList):
     for i in customerList[1:]:
         cursor.execute(f"""
         INSERT INTO 
-            Customers(number, firstname, lastname, gender, age) 
-            VALUES({i[0]}, {i[1]}, {i[2]}, {i[3]}, {i[4]})""")
+            `Customers` (`firstname`, `lastname`, `gender`, `age`) 
+            VALUES("{i[0]}", "{i[1]}", "{i[2]}", "{i[3]}")""")
 
 # Fill Store table from list
 # Skipping first entry as we dont want headlines as entries.
@@ -51,84 +53,139 @@ def fillStoreTable(storeList):
     for i in storeList[1:]:
         cursor.execute(f"""
         INSERT INTO 
-            Stores(number, name, address) 
-            VALUES({i[0]}, {i[1]}, {i[2]})""")
+            `Stores` (`name`, `address`) 
+            VALUES("{i[0]}", "{i[1]}")""")
 
+# Generates a random number between 1-5 where 1 is the most usual outcome.
+def randomOneToFive():
+    n = random.randint(1, 10)
+    if n > 5:
+        n = 1
+    return n
 
-# Tables are stored in a dictionary where, tablename becomes the key and
-# 'output' becomes the value.
-TABLES = {}
+def generateOrdersTable(AMOUNT_OF_ORDERS, MAX_ARTICLES_BOUGHT):
+    for order in range(1, AMOUNT_OF_ORDERS+1):
+        maxArticles = random.randint(1, MAX_ARTICLES_BOUGHT)
+        customer = random.randint(1, 1000)
+        store    = random.randint(1, 20)
 
-# The Customer table
-# Personal number as primary key.
-TABLES['Customers'] = ("""
-    CREATE TABLE `Customers`(
-        `number` BIGINT NOT NULL, 
-        `firstname` varchar(50), 
-        `lastname` varchar(50), 
-        `gender` varchar(1), 
-        `age` TINYINT, 
-        PRIMARY KEY (`number`)) 
-    ENGINE=InnoDB""")
+        cursor.execute(f"""
+            INSERT INTO 
+                `Orders` (`store`, `customer`)
+                VALUES("{store}", "{customer}")
+        """)
 
-# The Articles table
-# Article number as primary key. 
-# Article number gets auto incremented.
-TABLES['Articles'] = ("""
-    CREATE TABLE `Articles`(
-        `number` SMALLINT NOT NULL AUTO_INCREMENT, 
-        `name` varchar(100), 
-        `price` TINYINT, 
-        PRIMARY KEY (`number`))
-    ENGINE=InnoDB""")
+        for i in range(maxArticles):
+            article  = random.randint(1, 1000)
+            cursor.execute(f"""
+                INSERT INTO
+                    `OrdersArticles` (`order`, `article`, `amount`)
+                    Values("{order}", "{article}", "{randomOneToFive()}")
+                """)
 
-# The Stores table
-# Organisation number as primary key.
-TABLES['Stores'] = ("""
-    CREATE TABLE `Stores`(
-        `number` BIGINT NOT NULL, 
-        `name` varchar(250), 
-        `address` varchar(250), 
-        PRIMARY KEY (`number`))
-    ENGINE=InnoDB""")
+# Creates tables for 'Stores' database.
+def createTables():
+    # Tables are stored in a dictionary where, tablename becomes the key and
+    # 'output' becomes the value.
+    TABLES = {}
 
-# The Orders table. 
-# If entry for stores is removed. The order will be removed.
-# If entry for Customer is removed. The customer will be set to null.
-# Entry for Order detail
-TABLES['Orders'] = ("""
-    CREATE TABLE `Orders`(
-        `number` INT NOT NULL AUTO_INCREMENT, 
-        `store` BIGINT NOT NULL, 
-        `customer` BIGINT, 
-        `ordersarticles` INT NOT NULL, 
-        PRIMARY KEY (`number`,`store`), 
-        FOREIGN KEY (`store`) 
-            REFERENCES `Stores`(`number`) 
-            ON DELETE CASCADE, 
-        FOREIGN KEY (`customer`) 
-            REFERENCES `Customers`(`number`) 
-            ON DELETE SET NULL, 
-        FOREIGN KEY (`ordersarticles`) 
-            REFERENCES `OrdersArticles`(`number`)) 
-    ENGINE=InnoDB""")
+    # The Customer table
+    # Personal id as primary key.
+    TABLES['Customers'] = ("""
+        CREATE TABLE `Customers`(
+            `id` INT NOT NULL AUTO_INCREMENT, 
+            `firstname` varchar(50), 
+            `lastname` varchar(50), 
+            `gender` varchar(1), 
+            `age` TINYINT, 
+            PRIMARY KEY (`id`)) 
+        ENGINE=InnoDB""")
 
-# Table for OrdersArticles
-# Connects orders with its articles in line with BCNF. 
-# Order as foreign key. If order is removed, all OrderArticles
-# connected to it, are removed.
-# Article as foreign key. If an article is removed. All its
-# connected OrderArticles are removed. 
-TABLES['OrdersArticles'] = ("""
-    CREATE TABLE `OrderDetail`(
-        `number` INT NOT NULL AUTO_INCREMENT, 
-        `order` INT NOT NULL, 
-        `article` INT NOT NULL, 
-        PRIMARY KEY (`number`), 
-        FOREIGN KEY (`order`)
-            REFERENCES `Orders`(`number`)
-            ON DELETE CASCADE, 
-        FOREIGN KEY (`article`)
-            REFERENCES `Articles`(`number`)
-            ON DELETE CASCADE))
-    ENGINE=InnoDB""")
+    # The Articles table
+    # Article id as primary key. 
+    # Article id gets auto incremented.
+    TABLES['Articles'] = ("""
+        CREATE TABLE `Articles`(
+            `id` INT NOT NULL AUTO_INCREMENT, 
+            `name` varchar(100), 
+            `price` TINYINT, 
+            PRIMARY KEY (`id`)) 
+        ENGINE=InnoDB""")
+
+    # The Stores table
+    # Organisation id as primary key.
+    TABLES['Stores'] = ("""
+        CREATE TABLE `Stores`(
+            `id` INT NOT NULL AUTO_INCREMENT, 
+            `name` varchar(250), 
+            `address` varchar(250), 
+            PRIMARY KEY (`id`)) 
+        ENGINE=InnoDB""")
+
+    # The Orders table. 
+    # If entry for stores is removed. The order will be removed.
+    # If entry for Customer is removed. The customer will be set to null.
+    # Entry for Order detail
+    TABLES['Orders'] = ("""
+        CREATE TABLE `Orders`(
+            `id` INT NOT NULL AUTO_INCREMENT, 
+            `store` INT NOT NULL, 
+            `customer` INT NOT NULL, 
+            PRIMARY KEY (`id`), 
+            FOREIGN KEY (`customer`)
+                REFERENCES `Customers` (`id`),
+            FOREIGN KEY (`store`) 
+                REFERENCES `Stores` (`id`)) 
+        ENGINE=InnoDB""")
+
+    # Table for OrdersArticles
+    # Connects orders with its articles in line with BCNF. 
+    # Order as foreign key. If order is removed, all OrderArticles
+    # connected to it, are removed.
+    # Article as foreign key. If an article is removed. All its
+    # connected OrderArticles are removed. 
+    TABLES['OrdersArticles'] = ("""
+        CREATE TABLE `OrdersArticles`(
+            `id` INT NOT NULL AUTO_INCREMENT, 
+            `order` INT NOT NULL, 
+            `article` INT NOT NULL, 
+            `amount` INT NOT NULL, 
+            PRIMARY KEY (`id`),
+            FOREIGN KEY (`order`) 
+                REFERENCES `Orders` (`id`),
+            FOREIGN KEY (`article`)
+                REFERENCES `Articles` (`id`)) 
+        ENGINE=InnoDB""")
+    return TABLES
+
+TABLES = createTables()
+
+customers = createList('docs/customers.csv')
+articles = createList('docs/articles.csv')
+stores = createList('docs/stores.csv')
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor, DB_NAME)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+        
+        for table_name in TABLES:
+            table_description = TABLES[table_name]
+            print("Creating table {}: ".format(table_name))
+            cursor.execute(table_description)
+        
+        fillStoreTable(stores)
+        fillCustomerTable(customers)
+        fillArticleTable(articles)
+        generateOrdersTable(1000, 10)
+    else:
+        print(err)
+        exit(1)
+
+cnx.commit()
+cursor.close()
+cnx.close()
